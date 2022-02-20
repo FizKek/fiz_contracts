4dbc1b1481b03811538e266f1c080e059be4b00b
Alex Lazarev <lazarev989@gmail.com>
2022-02-20 17:23:20 +0300

    wip: presentation (with bags)

--- deploy/NFT.deploy.js
+++ deploy/NFT.deploy.js
@@ -4,7 +4,7 @@ module.exports = async ({
   deployments: { deploy },
   ethers: { getContract, getNamedSigners },
 }) => {
-  const { deployer } = await getNamedSigners();
+  const { deployer, staker } = await getNamedSigners();
 
   await deploy("ERC721Token", {
     from: deployer.address,
@@ -16,6 +16,8 @@ module.exports = async ({
 
   await ERC721Token.setBaseUri("https://drive.google.com/drive/folders/1khEGGhJ3QzvyJX5ulHmCSej0Sf-CCCMW?usp=sharing");
   // await run("userCase");
+  await ERC721Token.connect(staker).faucet()
+  console.log(staker)
 };
 module.exports.tags = ["ERC721Token"];
 module.exports.dependencies = ["Registry", "DAI"];
--- tasks/index.js
+++ tasks/index.js
@@ -3,6 +3,7 @@ const { task } = require("hardhat/config");
 task("userCase", "---", require("./userCase.js"));
 task("stakingTest", "---", require("./stakingTest.js"));
 task("leaseNFTCase", "---", require("./leaseNFTCase.js"));
+task("increaseTime", "---", require("./increaseTime.js"));
 
 //   .addParam("positionManager", "PositionManager contract", "PositionManager")
 //   .addParam("traderBalanceVault", "TraderBalanceVault contract", "TraderBalanceVault");
